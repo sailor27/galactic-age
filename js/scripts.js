2851715de970accb85ed8f6bfb9805ef5ee76ec8
Sailor Winkelman <sailorwinkelman@gmail.com>
2018-01-05 10:01:55 -0800

    Write toSeconds function to convert years to seconds

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
 // script test
-function Year(input, seconds){
-  this.input = input;
-  this.toSeconds();
-}
+// function Year(input, seconds){
+//   this.input = input;
+//   this.toSeconds();
+// }
 
-Year.prototype.toSeconds = function(){
-  return (this.input * 31,536,000);
+var toSeconds = function(years){
+  var seconds = Math.floor(years * 31536000);
+  console.log(seconds);
 };
