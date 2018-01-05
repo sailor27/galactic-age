78556d07124a7e61c0318d33706131adeb91daa8
Sailor Winkelman <sailorwinkelman@gmail.com>
2018-01-05 11:56:29 -0800

    Research Moment. Add birthday form to HTML.

--- build/js/app.js
+++ build/js/app.js
@@ -5,23 +5,48 @@ function Age(years, seconds){
   this.seconds = seconds;
 }
 
+//date constructor is built into javascript
+// function Date(year, month, day, hours, minutes, seconds, milliseconds);
+
 Age.prototype.toSeconds = function(){
   this.seconds = Math.floor(this.years * 31536000);
 };
 
-// $(document).ready(function() {
-//   $("form#age-form").submit(function(event){
-//     event.preventDefault();
-//     var inputtedAge = $("#age").val();
-//     var seconds = 2;
-//     var currentAge = new Age(inputtedAge, seconds);
-//     console.log(inputtedAge);
-//     console.log(currentAge);
-//     currentAge.toSeconds();
-//     console.log(currentAge);
-//   });
-// });
-exports.AgeModule = Age;
+$(document).ready(function() {
+  $("form#age-form").submit(function(event){
+    event.preventDefault();
+
+//assign year based on user input and seconds as a placeholder
+    var inputtedAge = $("#age").val();
+    var seconds = seconds;
+
+//make a new Age object
+    var currentAge = new Age(inputtedAge, seconds);
+
+//testing for Age constructor
+    console.log(inputtedAge);
+    console.log(currentAge);
+
+    currentAge.toSeconds();
+    console.log(currentAge);
+
+//testing for JS date constructor
+    var Xmas95 = new Date('December 25, 1995 23:15:30');
+    var day = Xmas95.getDate();
+    console.log(day);
+  });
+
+//testing for moment
+// var now = moment();
+// console.log(now);
+
+//
+var inputtedBirthday = $("#birthday").val();
+console.log(inputtedBirthday);
+
+
+});
+// exports.AgeModule = Age;
 
 },{}],2:[function(require,module,exports){
 //test1
--- js/form-interface.js
+++ js/form-interface.js
@@ -4,12 +4,35 @@ var Age = require('./../js/scripts.js').ageModule;
 $(document).ready(function() {
   $("form#age-form").submit(function(event){
     event.preventDefault();
-    var inputtedAge = $("#age").val();
-    console.log(inputtedAge);
 
-    var currentAge = new Age();
+//assign year based on user input and seconds as a placeholder
+    var inputtedAge = $("#age").val();
+    var seconds = seconds;
+
+//make a new Age object
+    var currentAge = new Age(inputtedAge, seconds);
+
+//testing for Age constructor
+    console.log(inputtedAge);
     console.log(currentAge);
+
     currentAge.toSeconds();
     console.log(currentAge);
+
+//testing for JS date constructor
+    var Xmas95 = new Date('December 25, 1995 23:15:30');
+    var day = Xmas95.getDate();
+    console.log(day);
   });
+
+//testing for moment
+// var now = moment();
+// console.log(now);
+
+  $("form#birthday-form").submit(function(event){
+    event.preventDefault();
+    var inputtedBirthday = $("#birthday").val();
+    console.log(inputtedBirthday);
+  });
+
 });
